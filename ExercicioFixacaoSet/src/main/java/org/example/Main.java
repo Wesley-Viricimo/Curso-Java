b67e72866699ccdfbe37e7b9c19399fa3a785e49
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-07-06 18:30:21 -0300

    Exercicio fixação set

---
+++ ExercicioFixacaoSet/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.example.entities.LogEntry;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String path = "c:\\temp\\in.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+            Set<LogEntry> set = new HashSet<>();
+
+            String line = br.readLine();
+            while (line != null) {
+                String[] fields = line.split(" ");
+                String username = fields[0];
+                Date moment = Date.from(Instant.parse(fields[1]));
+
+                set.add(new LogEntry(username, moment)); //Caso tenha algum log com usuário repetido, não será adicionado no set
+
+                line = br.readLine();
+            }
+
+            System.out.println("Total users: " + set.size());
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
