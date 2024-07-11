26f4b753dbeb9198f266c98bba1d67bffeb2d945
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-07-11 10:08:23 -0300

    Criando entity, resource, service e repository para product

---
+++ WebServiceSpringBoot/src/main/java/com/cursonelioalves/resources/ProductResource.java
@@ -0,0 +1,32 @@
+package com.cursonelioalves.resources;
+
+import com.cursonelioalves.entities.Product;
+import com.cursonelioalves.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController                          //Informando que essa classe será de um controlador rest, que receberá as requisições dos usuários
+@RequestMapping(value = "/products")     //Informando endpoint para que seja possível acessar requisições da rota products
+public class ProductResource {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public ResponseEntity<List<Product>> findAll() {
+        List<Product> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Product> findById(@PathVariable Long id) {
+        Product obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
