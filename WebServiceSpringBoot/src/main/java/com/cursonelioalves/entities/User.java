4da57193dfbc38b1b5eb8317683745bd20e7c683
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-07-10 22:42:47 -0300

    Criando entidade pedido e ajustando relacionamento com tabela usuários

--- WebServiceSpringBoot/src/main/java/com/cursonelioalves/entities/User.java
+++ WebServiceSpringBoot/src/main/java/com/cursonelioalves/entities/User.java
@@ -3,6 +3,8 @@ package com.cursonelioalves.entities;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +19,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")                 //Um usuário para muitos pedidos
+    private List<Order> orders = new ArrayList<>(); //Um usuário poderá ter vários pedidos
 
     public User() {}
 
@@ -62,6 +67,10 @@ public class User implements Serializable {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
