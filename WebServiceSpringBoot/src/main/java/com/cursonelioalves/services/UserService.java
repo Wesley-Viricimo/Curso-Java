04c9a1dab475ea57f35a6e1b4dd5b8874e03e405
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-07-14 22:55:10 -0300

    Implementando exception handler na busca de usuário por id

--- WebServiceSpringBoot/src/main/java/com/cursonelioalves/services/UserService.java
+++ WebServiceSpringBoot/src/main/java/com/cursonelioalves/services/UserService.java
@@ -2,6 +2,7 @@ package com.cursonelioalves.services;
 
 import com.cursonelioalves.entities.User;
 import com.cursonelioalves.repositories.UserRepository;
+import com.cursonelioalves.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
