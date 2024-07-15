828eadb8c73e79d51a5e846c5bf67364448a242e
Wesley-Viricimo <wesley.viricimo.silva@outlook.com>
2024-07-14 23:09:43 -0300

    Exception handling delete

--- WebServiceSpringBoot/src/main/java/com/cursonelioalves/services/UserService.java
+++ WebServiceSpringBoot/src/main/java/com/cursonelioalves/services/UserService.java
@@ -2,8 +2,11 @@ package com.cursonelioalves.services;
 
 import com.cursonelioalves.entities.User;
 import com.cursonelioalves.repositories.UserRepository;
+import com.cursonelioalves.services.exceptions.DatabaseException;
 import com.cursonelioalves.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -29,7 +32,14 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User obj) {
